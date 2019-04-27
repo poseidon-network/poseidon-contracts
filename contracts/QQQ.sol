b73503db37558f8e126c6d83645448f4e2bb78a5
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-04-27 22:32:31 +0800

    Change to QQQ contract

---
+++ contracts/QQQ.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+contract QQQ is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+    uint private INITIAL_SUPPLY = 10000e18;
+
+    constructor() public
+        ERC20Burnable()
+        ERC20Mintable()
+        ERC20Detailed("QQQToken", "QQQ", 18)
+        ERC20()
+    {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
