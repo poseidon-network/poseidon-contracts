3ca388775073366b397f65bc37b5b00c5a4a8049
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-04-27 23:30:45 +0800

    Remove unused import

--- contracts/QQQ.sol
+++ contracts/QQQ.sol
@@ -4,7 +4,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
 contract QQQ is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
